53feccb5d734b4f31a03c310f310b53537baa7b3
jaber saraj <jaber2x@gmail.com>
2024-08-29 19:43:22 +0330

    Connect EmailJS to the project form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,8 +1,26 @@
 
-
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function ContactForm() {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs
+            .sendForm('service_0ddssn9', 'template_44q61jg', form.current, {
+                publicKey: 'rtB0jEwqQQDZWNFwk',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
     return (
 
         <div className='flex justify-center items-center h-screen'>
@@ -11,18 +29,20 @@ function ContactForm() {
                     <h3>Post Comments</h3>
                 </div>
                 <div>
-                    <div className='py-3'>
-                        <input type='text' name='name' placeholder='Your Name:' className='w-full rounded-md p-2 focus:outline-none'></input>
-                    </div>
-                    <div className='py-3'>
-                        <input type='email' name='email' placeholder='Your Email' className='w-full rounded-md p-2 focus:outline-none'></input>
-                    </div>
-                    <div className='py-3'>
-                        <textarea name='message' placeholder='Your Message' rows='6' className='w-full rounded-md p-2 focus:outline-none'></textarea>
-                    </div>
-                    <div className='flex justify-center'>
-                        <button className='bg-gray-300 w-full rounded-md p-2 font-semibold'>Send</button>
-                    </div>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <div className='py-3'>
+                            <input type='text' name='name' placeholder='Your Name:' className='w-full rounded-md p-2 focus:outline-none'></input>
+                        </div>
+                        <div className='py-3'>
+                            <input type='email' name='email' placeholder='Your Email' className='w-full rounded-md p-2 focus:outline-none'></input>
+                        </div>
+                        <div className='py-3'>
+                            <textarea name='message' placeholder='Your Message' rows='6' className='w-full rounded-md p-2 focus:outline-none'></textarea>
+                        </div>
+                        <div className='flex justify-center'>
+                            <button className='bg-gray-300 w-full rounded-md p-2 font-semibold shadow-[0_0px_5px_3px_rgba(0,0,0,0.1)]' >Send</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
